579981a13627358fe95f19d65fe4f2c6127ff926
Clay Whitley <clay.aaron.whitley@gmail.com>
2013-08-06 17:39:15 -0700

    add basic routing, models, and views for MVP

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,39 @@
 get '/' do
   # Look in app/views/index.erb
+  @notes = Note.all
   erb :index
 end
+
+# Edit
+get '/notes/edit/:id' do
+  @note = Note.find(params[:id])
+  erb :edit
+end
+
+# Create
+post '/notes' do
+  Note.create(params[:note])
+  erb :index
+end
+
+# Read
+get '/notes/:id' do
+  @note = Note.find(params[:id])
+  erb :view_note
+end
+
+# Update
+put '/notes/:id' do
+  note = Note.find(params[:id])
+  note.update_attributes(params[:note])
+  redirect '/'
+end
+
+# Destroy
+delete '/notes/:id' do
+  note = Note.find(params[:id])
+  note.destroy
+  redirect '/'
+end
+
+
